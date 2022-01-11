df531e4e1787452e439168944229be1951d4e869
Vlad4uni <vtrayanov18@gmail.com>
2022-01-11 15:50:47 +0000

    added value checks

--- Fadecandy_2400-main/examples/UI demo.py
+++ Fadecandy_2400-main/examples/UI demo.py
@@ -1,8 +1,5 @@
 value = input("Welcome to the menu.Options are listed below:\n\t 1.Roll \n\t 2.Sweep \n\t 3.Scroll \n\t Type the number of your choice and press Enter.")
 
-#print("The value of your input is :", value)
-#print(f'it is of type {type(value)}.')
-
 def func1(val):
     return val**val
 def func2(val):
@@ -13,12 +10,13 @@ def func3(val):
 while True:
     if value.isdigit() == True: #.isdigit()
         value = int(value)
-        break #on correct value datatype:exit the loop
+        if value > 3 or value < 1 :#if value is outside of our range :
+            value = input ("Please input a number between 1 and 3 .")
+            continue #skip rest of loop , start from isdigit() check again
+        else:
+             break #on correct value datatype:exit the loop
     else:
-        value =input("invalid input , please provide an integer") #ask for a new value
-
-#print("The converted is:", value)
-#print(f'it is of type {type(value)}.')
+        value =input("Invalid input ! Please provide an integer.") #ask for a new value
 
 #compare numeric value to choices available , perform associiated function or sequence
 if value == 1:
