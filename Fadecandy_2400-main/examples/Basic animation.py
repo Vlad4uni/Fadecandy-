453fe1ebff698be7d3c26f06ee8de4a85d776c89
Vlad4uni <vtrayanov18@gmail.com>
2022-01-11 17:34:38 +0000

    Scroll from in to the out and inverse

--- Fadecandy_2400-main/examples/Basic animation.py
+++ Fadecandy_2400-main/examples/Basic animation.py
@@ -11,13 +11,13 @@ client.put_pixels(leds)
 #for led in leds: #pick out an element:led (255,255,255)
 for led in range(len(leds)):
     leds [led] = (255,0,0)
-    time.sleep(.1)
+    time.sleep(0.1)
     client.put_pixels(leds)
 
 led = 0
 while led < 60 :
     leds [59 - led] = (0,255,0)
-    time.sleep(.1)
+    time.sleep(0.1)
     client.put_pixels(leds)
     led = led + 1 #or reverse if you want
 
@@ -30,15 +30,19 @@ while led < 60 :
 
 led = 0
 while led < 60:#scroll all rows at the same time
-    for rows in range (6):
+    for rows in range (3): #first three rows left to right
         leds[led + rows * 60] = (0,0,255)
-#    leds[led] = (0,0,255) #row 1- 0-60
-#    leds[led+60] = (0,0,255) #row 2 - 0+60 - 59+60 (60-119)
-#    leds[led+120] = (0,0,255)
-#    leds[led+180] = (0,0,255)#this can be done in 2 lines instead of 6
-#    leds[led+240] = (0,0,255)
-#    leds[led+300] = (0,0,255)
+    for rows in range (3,6):#last three rows reversed (righty to left )
+        leds[59 - led + rows*60] = (50,50,255)
     client.put_pixels(leds)
-    time.sleep(.1)
+    time.sleep(0.1)
     led = led + 1
 
+led = 0
+while led <30 :
+    for rows in range (6):
+        leds [ led + rows * 30 ] = (200,100,100)
+        leds [ 59 - led + rows*30] = (200,100,100)
+    client.put_pixels(leds)
+    time.sleep(0.1)
+    led = led + 1    
