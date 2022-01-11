bfb678e3e12fce6668d10e7ac9a159854528482a
Vlad4uni <vtrayanov18@gmail.com>
2022-01-11 16:03:53 +0000

    updated basic animation 1

--- Fadecandy_2400-main/examples/Basic animation.py
+++ Fadecandy_2400-main/examples/Basic animation.py
@@ -5,15 +5,11 @@ import random
 leds = [(255,255,255)]*360 #white
 
 client = opc.Client('localhost:7890')
-
-leds[10] =(255,0,0)
-
-time.sleep(3)
 client.put_pixels(leds)
 client.put_pixels(leds)
 
-leds[10+60] =(255,0,0)
-
-time.sleep(3)
-client.put_pixels(leds)
-client.put_pixels(leds)
+#for led in leds: #pick out an element:led (255,255,255)
+for led in range(len(leds)):
+    leds [led] = (255,0,0)
+    time.sleep(.1)
+    client.put_pixels(leds)
