659c295f0a29817331d5e0e64b0fb425beefe8ae
Vlad4uni <vtrayanov18@gmail.com>
2022-01-11 15:31:10 +0000

    options list added

--- Fadecandy_2400-main/examples/UI demo.py
+++ Fadecandy_2400-main/examples/UI demo.py
@@ -1,3 +1,29 @@
-value = input()
+value = input("Welcome to the menu.Options are listed below:\n\t 1.Roll \n\t 2.Sweep \n\t 3.Scroll \n\t Type the number of your choice and press Enter.")
 
-print(value)
+#print("The value of your input is :", value)
+#print(f'it is of type {type(value)}.')
+
+def func1(val):
+    return val**val
+def func2(val):
+    return val**val
+def func3(val):
+    return val**val
+
+while True:
+    if value.isdigit() == True: #.isdigit()
+        value = int(value)
+        break #on correct value datatype:exit the loop
+    else:
+        value =input("invalid input , please provide an integer") #ask for a new value
+
+#print("The converted is:", value)
+#print(f'it is of type {type(value)}.')
+
+#compare numeric value to choices available , perform associiated function or sequence
+if value == 1:
+    func1(value)
+elif value == 2:
+    func2(value)
+elif value == 3:
+    func3(value)
