fc6a201a8520fb13562ed7cd5c85524bece4c680
Vlad4uni <vtrayanov18@gmail.com>
2022-01-18 17:44:15 +0000

    New Animation
    Creating  a new animation , might use it if I decide to change my project idea.

---
+++ Fadecandy_2400-main/examples/Chase.py
@@ -0,0 +1,12 @@
+import opc
+import time
+
+numLEDs = 360
+client = opc.Client('localhost:7890')
+
+while True:
+	for i in range(numLEDs):
+		pixels = [ (0,0,0) ] * numLEDs
+		pixels[i] = (255, 255, 255)
+		client.put_pixels(pixels)
+		time.sleep(0.01)
