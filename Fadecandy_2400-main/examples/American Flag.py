f28305743f52bf97c404ee4b0f709cdbd38a32f3
Vlad4uni <vtrayanov18@gmail.com>
2022-01-18 14:53:43 +0000

    Snake added
    attempted to add the snake (not finished)

--- Fadecandy_2400-main/examples/American Flag.py
+++ Fadecandy_2400-main/examples/American Flag.py
@@ -11,20 +11,21 @@ client.put_pixels(leds)
 #Animation 2
 
 led = 0
-while led < 60:#scroll all rows at the same time
-    for rows in range (1): #first row red 
-        leds[led + rows * 60] = (255,0,0)
-    for rows in range (1,2):#last three rows reversed (righty to left )
-        leds[59 - led + rows*60] = (255,255,255)
-    for rows in range (2,3): #first three rows left to right
-        leds[led + rows * 60] = (255,0,0)
-    for rows in range (3,4):#last three rows reversed (righty to left )
-        leds[59 - led + rows*60] = (255,255,255)
-    for rows in range (4,5): #first three rows left to right
-        leds[led + rows * 60] =  (255,0,0)
-    for rows in range (5,6):#last three rows reversed (righty to left )
-        leds[59 - led + rows*60] = (255,255,255)
-    client.put_pixels(leds)
-    time.sleep(0.1)
-    led = led + 1
+led [244] = (255,0,0)
+#while led < 60:#scroll all rows at the same time
+#    for rows in range (1): #first row red 
+#        leds[led + rows * 60] = (255,0,0)
+#    for rows in range (1,2):#last three rows reversed (righty to left )
+#        leds[59 - led + rows*60] = (255,255,255)
+#    for rows in range (2,3): #first three rows left to right
+#        leds[led + rows * 60] = (255,0,0)
+#    for rows in range (3,4):#last three rows reversed (righty to left )
+#        leds[59 - led + rows*60] = (255,255,255)
+#    for rows in range (4,5): #first three rows left to right
+#        leds[led + rows * 60] =  (255,0,0)
+#    for rows in range (5,6):#last three rows reversed (righty to left )
+#        leds[59 - led + rows*60] = (255,255,255)
+#    client.put_pixels(leds)
+#    time.sleep(0.1)
+#    led = led + 1
 
--- Fadecandy_2400-main/examples/Basic animation.py
+++ Fadecandy_2400-main/examples/Basic animation.py
@@ -14,19 +14,19 @@ client.put_pixels(leds)
 #        time.sleep(0.1)
 #        client.put_pixels(leds)
 
-led = 0 # For green light going right to left
-while led < 60 :
-    leds [59 - led] = (0,255,0)
-    time.sleep(0.1)
-    client.put_pixels(leds)
-    led = led + 1 #or reverse if you want
+#led = 0 # For green light going right to left
+#while led < 60 :
+#    leds [59 - led] = (0,255,0)
+#    time.sleep(0.1)
+#    client.put_pixels(leds)
+#    led = led + 1 #or reverse if you want
 
-led = 0
-while led < 60 :
-    leds [led] = (0,255,0)
-    time.sleep(.1)
-    client.put_pixels(leds)
-    led = led - 1 #or reverse if you want
+#led = 0
+#while led < 60 :
+#    leds [led] = (0,255,0)
+#    time.sleep(.1)
+#    client.put_pixels(leds)
+#    led = led - 1 #or reverse if you want
 
 #led = 0
 #while led < 60:#scroll all rows at the same time
@@ -38,13 +38,25 @@ while led < 60 :
 #    time.sleep(0.1)
 #    led = led + 1
 
-led = 0
-while led <30 :
-    for rows in range (6):
-        leds [ led + rows * 30 ] = (200,100,100)
-        leds [ 59 - led + rows*30] = (200,100,100)
-    client.put_pixels(leds)
-    time.sleep(0.1)
-    led = led + 1    
+#led = 0
+#while led <30 :
+#    for rows in range (6):
+#        leds [ led + rows * 30 ] = (200,100,100)
+#        leds [ 59 - led + rows*30] = (200,100,100)
+#    client.put_pixels(leds)
+#    time.sleep(0.1)
+#    led = led + 1    
 
+while True:
+    for led in range(0,360,60):
+        leds = [(255,255,255)]*360
+        leds[355-led] = (0,0,255)
+        leds[355-led+1] = (0,0,255)
+        leds[355-led+2] = (0,0,255)
+        leds[355-led+3] = (0,0,255)
+        leds[355-led+4] = (0,0,255)
+        if led == 355:
+           led = 0
+        client.put_pixels(leds)
+        time.sleep(0.02)
 
