1a1762589d3a9a0ae31ce434e84d49026cd067c5
Vlad4uni <vtrayanov18@gmail.com>
2022-01-23 21:42:41 +0000

    Update gui try.py

--- Fadecandy_2400-main/examples/gui try.py
+++ Fadecandy_2400-main/examples/gui try.py
@@ -10,12 +10,17 @@ lbl = Label(window, text="Hello", font=("Arial Bold", 40))
 
 lbl.grid(column=0, row=0)
 
-def clicked():
+def start():
 
     lbl.configure(text="What is your favourite country ?",font=("Arial Bold", 20))
 
-btn = Button(window, text="Shaw we begin ?", bg="green", fg="orange" ,command=clicked)
-
+btn = Button(window, text="Are you ready to start ?", bg="green", fg="orange" ,command=start)
 btn.grid(column=1, row=2)
+
+def enter():
+    lbl.confiigure(text="Please enter your favorite country in full capitals !",font=("Arial Bold", 20))
+
+btn2 = Button(window, text="Enter", bg="green", fg="orange" ,command=enter)
+btn2.grid(column=1, row=4)
 
 window.mainloop()
