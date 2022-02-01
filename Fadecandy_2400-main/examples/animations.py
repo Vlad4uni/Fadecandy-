12711061297fc728c15c00698c074cb031df4d1e
Vlad4uni <vtrayanov18@gmail.com>
2022-02-01 14:06:30 +0000

    Added the End Animation to the animations package
    Previously I had forgotten to add the final the final animation to the sequence .
    
    Also added a clear at the end of the sequence so that once the sequence is fully executed the lights would reset and wait for the user to start the process again if wanted .

--- Fadecandy_2400-main/examples/animations.py
+++ Fadecandy_2400-main/examples/animations.py
@@ -143,3 +143,22 @@ def flags(client):
     spanish_flag(client)
     client.put_pixels(empty)
     time.sleep(1)
+
+
+def end(client):
+    colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 0, 255),
+              (0, 255, 255), (255, 255, 0)]
+    for i in range(3):
+        strips = []
+        for i in range(total_strips):
+            strips = [[(0, 0, 0) for led in range(leds_per_strip)]
+                      for strip in range(total_strips)]
+            strips[i] = [colors[i] for pixel in range(leds_per_strip)]
+            pixels = [j for sub in strips for j in sub]
+            client.put_pixels(pixels, channel=0)
+            time.sleep(0.3)
+
+
+def clear(client):
+    empty = [(0, 0, 0)] * 360
+    client.put_pixels(empty, channel=0)
