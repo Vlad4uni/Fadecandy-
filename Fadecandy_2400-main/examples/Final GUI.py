57e805ab0a60ce04f4cb751eeeb01491945cf0e9
Vlad4uni <vtrayanov18@gmail.com>
2022-02-07 11:17:03 +0000

    Improved the comments
    Added new comments to my work while also improving some of the pre-existing comments .

--- Fadecandy_2400-main/examples/Final GUI.py
+++ Fadecandy_2400-main/examples/Final GUI.py
@@ -5,7 +5,7 @@ from animations import *
 from multiprocessing import Process
 from threading import Thread
 
-
+#function that i can call upon once it comes time to create an animation
 def animate(process, animation_func, client):
     if process:
         process.terminate()
@@ -20,12 +20,12 @@ def main():
     animation_process = None
     window = tk.Tk()
     window.title('Welcome to Vlad\'s World quiz')
-    window.geometry('450x200')
+    window.geometry('450x200')# size of Tkinter box
 
     var_lbl_main = tk.StringVar()
     lbl_main = tk.Label(textvariable=var_lbl_main,
                         font=('Arial', 18),
-                        justify=tk.CENTER,
+                        justify=tk.CENTER, #setting the size and locatiion of 
                         width=25,
                         height=2,
                         bg='blue')
@@ -45,22 +45,22 @@ def main():
 
     def submit_answer():
         answer = txt_answer.get().lower()
-        if answer in ['bulgaria', 'america', 'spain']:
+        if answer in ['bulgaria', 'america', 'spain']:# checks the users input against the three countries that I have entered to be my favourite countries
             var_lbl_reply.set(
-                'How amazing is that, we have matching tastes when it comes to countries, these are my favorite countries: '
+                'How amazing is that, we have matching tastes when it comes to countries, these are my favorite countries: ' # response if the country appears to be one of my favourite three
             )
         else:
             var_lbl_reply.set(
-                'It appears that we do not have the same taste when it comes to countries, these are my favourite countries'
+                'It appears that we do not have the same taste when it comes to countries, these are my favourite countries' # response if the country appears not to be one of my favourite three
             )
 
         lbl_main.place_forget()
         lbl_reply.place(x=50, y=10)
 
-        # show countries
+        # at the same time the appropriate text appears on the Tkinter window , the 3 animations for the countries will begin in the simulations window
         animate(animation_process, flags, client)
 
-        def end_animation():
+        def end_animation():#end of participation notice appears on Tkinter while the end animation plays at the same time 
             time.sleep(20)
             var_lbl_reply.set('Thank you for participating in this quiz.')
             animate(animation_process, end, client)
@@ -70,19 +70,19 @@ def main():
         # play final animation
         Thread(target=end_animation).start()
 
-    def reset():
-        btn_enter.place_forget()
+    def reset(): #overall reset of both Tkinter window and the simulation window
+        btn_enter.place_forget() 
         txt_answer.place_forget()
         lbl_reply.place_forget()
-
+#previous buttons disapear and new buttons appear (initial buttons)
         var_lbl_main.set('Hello')
         lbl_main.place(x=50, y=10)
 
-        btn_ready.configure(text='Try again')
+        btn_ready.configure(text='Try again')#for the process to start again the user has to press this button because otherwise the process will just stop here and wait for a human response
         btn_ready.place(x=50, y=100)
 
-        animate(animation_process, globe, client)
-
+        animate(animation_process, globe, client) #similar to the Tkinter window , the animations that appear in the simulation window are also all reset 
+                                                                                  #therefore  the animations cycle begins again with the globe 
     def start():
         btn_ready.place_forget()
 
