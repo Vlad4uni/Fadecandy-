a4e3f62816e546a6919f7306e8a50506cfcf45da
Vlad4uni <vtrayanov18@gmail.com>
2022-02-07 09:57:00 +0000

    Added extra comments and improved the already existing comments for the Bulgarian flag

--- Fadecandy_2400-main/examples/bulgarian_flag.py
+++ Fadecandy_2400-main/examples/bulgarian_flag.py
@@ -2,7 +2,7 @@ import opc
 import time
 import random
 
-leds = [(192,192,192)]*360 #silver
+leds = [(192,192,192)]*360 #silver -starting value for leds
 
 client = opc.Client('localhost:7890')
 client.put_pixels(leds)
@@ -12,11 +12,11 @@ client.put_pixels(leds)
 
 led = 0
 while led < 60:#scroll all rows at the same time
-    for rows in range (2): #first three rows left to right
+    for rows in range (2): #first two rows left to right
         leds[led + rows * 60] = (255,255,255)
-    for rows in range (2,4): #first three rows left to right
+    for rows in range (2,4): #the next two row go inwards - right to left
         leds[59 - led + rows*60] = (34,139,34)
-    for rows in range (4,6): #first three rows left to right
+    for rows in range (4,6): #last two rows travel from the left to the right
         leds[led + rows * 60] = (255,0,0)
     client.put_pixels(leds)
     time.sleep(0.1)
